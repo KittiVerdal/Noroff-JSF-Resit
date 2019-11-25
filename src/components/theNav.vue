93eff110c9a46f83ac7cd77999a0e2cf2b53b1a7
Kitti Verdal <kitti@jbl.no>
2019-11-25 13:57:19 +0100

    Navbar

--- src/components/theNav.vue
+++ src/components/theNav.vue
@@ -1,17 +1,45 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="danger">
-      <b-navbar-brand href="#">Pokemon</b-navbar-brand>
+      <b-navbar-brand href="#">
+        <img src="./../assets/pokemon-logo.png" alt="pokemon logo" class="[ navheader_logo ]" />
+      </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#">Home</b-nav-item>
-          <b-nav-item href="#">Grass</b-nav-item>
-          <b-nav-item href="#">Contact</b-nav-item>
+        <b-navbar-nav class="[ navheader_list ]">
+          <b-nav-item class="[ navheader_list--item ]" href="#">Home</b-nav-item>
+          <b-nav-item class="[ navheader_list--item ]" href="#">Grass</b-nav-item>
+          <b-nav-item class="[ navheader_list--item ]" href="#">Contact</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.navheader {
+  &_logo {
+    width: 180px;
+  }
+  &_list {
+    font-weight: 900;
+    margin-left: 50px;
+    font-size: 24px;
+    margin-top: 30px;
+    &--item {
+      margin-right: 20px;
+    }
+  }
+}
+.navbar-dark .navbar-nav .nav-link {
+  color: yellow;
+  &:hover {
+    color: white;
+  }
+  &:focus {
+    color: white;
+  }
+}
+</style>
